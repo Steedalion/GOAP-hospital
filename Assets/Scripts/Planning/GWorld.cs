c127fb39a86972cbd9f98aadec9c3148d0a55469
(대학원생 기계공학과(공과대학)) CLINT ALEX STEED <steed@unist.ac.kr>
2021-05-06 17:28:09 +0900

    Imported Unity Toolbox to use singleton.

---
+++ Assets/Scripts/Planning/GWorld.cs
@@ -0,0 +1,16 @@
+﻿using UnityToolbox.Tools;
+
+namespace Planning
+{
+    public class GWorld:Singleton<GWorld>
+    {
+        public WorldStates WorldStates { get; }
+
+        public GWorld()
+        {
+            WorldStates = new WorldStates();
+        }
+        
+        
+    }
+}
---
+++ Assets/Scripts/Planning/PlanningEditTests/GWorldTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace Planning.PlanningEditTests
+{
+    public class GWorldTests
+    {
+        [Test]
+        public void ShouldNotBeNull()
+        {
+            Assert.IsNotNull(GWorld.Instance());
+        }
+
+        [Test]
+        public void CanAccessWorld()
+        {
+            Assert.IsNotNull(GWorld.Instance().WorldStates);
+        }
+    }
+}
